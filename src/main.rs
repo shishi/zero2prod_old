fcd78b8f981be35e0aa1a1287ca3aa1222dbf467
Shigenobu Nishikawa <shishi@srevo.net>
2021-11-04 02:33:23 +0900

    finish chapter 3.5

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use zero2prod::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
